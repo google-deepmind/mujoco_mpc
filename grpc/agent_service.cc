google-deepmind/mujoco_mpc

commit 5a4a838600cf531cdb50feddef26e4fdb76e2988

grpc AgentService: Use correct time step for stepping, different from planning.
The Step method was stepping the model with the planning timestep instead of the physics timestep.

PiperOrigin-RevId: 528760549
Change-Id: I235dbf96a2b8a517960bbf8569360bbf89f9cfe3

grpc/agent_service.cc
@@ -53,11 +53,14 @@ using ::agent::StepRequest;
 using ::agent::StepResponse;
 
 mjpc::Task* task = nullptr;
+// model used for physics
 mjModel* model = nullptr;
+// model used for planning, owned by the Agent instance.
+mjModel* agent_model = nullptr;
 
 void residual_sensor_callback(const mjModel* m, mjData* d, int stage) {
   // with the `m == model` guard in place, no need to clear the callback.
-  if (m == model) {
+  if (m == agent_model || m == model) {
     if (stage == mjSTAGE_ACC) {
       task->Residual(m, d, d->sensordata);
     }
@@ -132,7 +135,13 @@ grpc::Status AgentService::Init(grpc::ServerContext* context,
   agent_.Reset();
 
   task = agent_.ActiveTask();
-  model = agent_.GetModel();
+  CHECK_EQ(agent_model, nullptr)
+      << "Multiple instances of AgentService detected.";
+  agent_model = agent_.GetModel();
+  // copy the model before agent model's timestep and integrator are updated
+  CHECK_EQ(model, nullptr)
+      << "Multiple instances of AgentService detected.";
+  model = mj_copyModel(nullptr, agent_model);
   data_ = mj_makeData(model);
   mjcb_sensor = residual_sensor_callback;
 
@@ -146,8 +155,10 @@ grpc::Status AgentService::Init(grpc::ServerContext* context,
 
 AgentService::~AgentService() {
   if (data_) mj_deleteData(data_);
-  // no need to delete model and task, since they're owned by agent_.
+  if (model) mj_deleteModel(model);
   model = nullptr;
+  // no need to delete agent_model and task, since they're owned by agent_.
+  agent_model = nullptr;
   task = nullptr;
   mjcb_sensor = nullptr;
 }
@@ -338,7 +349,7 @@ grpc::Status AgentService::SetTaskParameters(
           std::ostringstream error_string;
           error_string << "Parameter " << name
                        << " not found in task.  Available names are:\n";
-          auto agent_model = agent_.GetModel();
+          auto* agent_model = agent_.GetModel();
           for (int i = 0; i < agent_model->nnumeric; i++) {
             std::string_view numeric_name(agent_model->names +
                                           agent_model->name_numericadr[i]);
@@ -394,7 +405,7 @@ grpc::Status AgentService::SetCostWeights(
       std::ostringstream error_string;
       error_string << "Weight '" << name
                    << "' not found in task. Available names are:\n";
-      auto agent_model = agent_.GetModel();
+      auto* agent_model = agent_.GetModel();
       for (int i = 0; i < agent_model->nsensor &&
                       agent_model->sensor_type[i] == mjSENS_USER;
            i++) {

grpc/agent_service_test.cc
@@ -186,7 +186,7 @@ TEST_F(AgentServiceTest, Step_AdvancesTime) {
     EXPECT_TRUE(status.ok()) << status.error_message();
   }
 
-  {
+  for (int i = 0; i < 3; i++) {
     grpc::ClientContext context;
     agent::StepRequest request;
     agent::StepResponse response;
@@ -204,7 +204,9 @@ TEST_F(AgentServiceTest, Step_AdvancesTime) {
     EXPECT_TRUE(status.ok()) << status.error_message();
     final_state = response.state();
   }
-  EXPECT_GT(final_state.time(), initial_state.time());
+  double cartpole_timestep = 0.001;
+  EXPECT_DOUBLE_EQ(final_state.time() - initial_state.time(),
+                   3 * cartpole_timestep);
 }
 
 TEST_F(AgentServiceTest, SetTaskParameters_Numeric) {

commit 1666953fa699f67ee2fb95e68a04975d25c968cd

Fix #includes in agent_service.cc.
5a4a838600cf531cdb50feddef26e4fdb76e2988 broke the CMake build by adding an unnecessary include.

PiperOrigin-RevId: 529493134
Change-Id: I2ad3618823a991e39b09afea568831c061ac277a

grpc/agent_service.cc
@@ -20,6 +20,7 @@
 #include <string_view>
 #include <vector>
 
+#include <absl/log/check.h>
 #include <absl/status/status.h>
 #include <absl/strings/match.h>
 #include <absl/strings/str_format.h>
@@ -28,7 +29,6 @@
 #include <grpcpp/support/status.h>
 #include <mujoco/mujoco.h>
 #include "grpc/agent.pb.h"
-#include "third_party/mujoco_mpc/grpc/agent.proto.h"
 #include "mjpc/task.h"
 
 namespace agent_grpc {
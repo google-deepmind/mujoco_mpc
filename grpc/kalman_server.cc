27d788b9bb791e1bdde8b07f70ea9ade2c605b87
taylor howell <taylor.athaniel.howell@gmail.com>
2023-08-01 16:03:24 -0600

    KALMAN -> Kalman

--- grpc/kalman_server.cc
+++ grpc/kalman_server.cc
@@ -12,7 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-// Startup code for `KALMAN` server.
+// Startup code for `Kalman` server.
 
 #include <cstdint>
 #include <memory>

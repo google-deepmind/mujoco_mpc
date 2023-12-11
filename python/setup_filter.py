6694015eeb6b79c122c1e1c8e06a53737f0cd254
Vince Kurtz <vjkurtz@gmail.com>
2023-12-10 19:15:27 -0800

    build python bindings in release mode

--- python/setup_filter.py
+++ python/setup_filter.py
@@ -211,7 +211,7 @@ class BuildCMakeExtension(build_ext.build_ext):
   def _configure_and_build_filter_server(self):
     """Check for CMake."""
     cmake_command = "cmake"
-    build_cfg = "Debug"
+    build_cfg = "Release"
     mujoco_mpc_root = Path(__file__).parent.parent
     mujoco_mpc_build_dir = mujoco_mpc_root / "build"
     cmake_configure_args = [

411db1c9a2fafbd544626b4898b268f8853eeba9
taylor howell <taylor.athaniel.howell@gmail.com>
2023-12-21 11:19:31 -0700

    fix header define

--- mjpc/planners/sampling/planner.h
+++ mjpc/planners/sampling/planner.h
@@ -12,8 +12,8 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-#ifndef MJPC_PLANNERS_SAMPLING_OPTIMIZER_H_
-#define MJPC_PLANNERS_SAMPLING_OPTIMIZER_H_
+#ifndef MJPC_PLANNERS_SAMPLING_PLANNER_H_
+#define MJPC_PLANNERS_SAMPLING_PLANNER_H_
 
 #include <mujoco/mujoco.h>
 
@@ -157,4 +157,4 @@ class SamplingPlanner : public RankedPlanner {
 
 }  // namespace mjpc
 
-#endif  // MJPC_PLANNERS_SAMPLING_OPTIMIZER_H_
+#endif  // MJPC_PLANNERS_SAMPLING_PLANNER_H_

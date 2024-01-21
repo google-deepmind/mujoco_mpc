013f3cee8102dff848bbcd4e51fb224cb40dc5f3
taylor howell <taylor.athaniel.howell@gmail.com>
2024-01-21 12:31:39 -0700

    in progress

--- mjpc/tasks/bimanual/bimanual.cc
+++ mjpc/tasks/bimanual/bimanual.cc
@@ -41,25 +41,82 @@ void Bimanual::ResidualFn::Residual(const mjModel* model, const mjData* data,
   // counter
   int counter = 0;
 
-  // // ----- Effort ----- //
-  // mju_copy(residual + counter, data->actuator_force, model->nu);
-  // counter += model->nu;
+  // end effector positions
+  double* ee_left = SensorByName(model, data, "ee_left");
+  double* ee_right = SensorByName(model, data, "ee_right");
 
-  // ----- Block position ----- //
+  // base positions
+  double base_left[3] = {-0.5, 0.0, 0.0};
+  double base_right[3] = {0.5, 0.0, 0.0};
+
+  // block position
   double* block_pos = SensorByName(model, data, "block_pos");
+  
+  // goal position
   double* goal_pos = SensorByName(model, data, "goal_pos");
-  mju_sub3(residual + counter, block_pos, goal_pos);
-  counter += 3;
+
+  // -- distances -- //
+
+  // left ee to block
+  double left_block_err[3];
+  mju_sub3(left_block_err, ee_left, block_pos);
+  double left_block_dist = mju_norm3(left_block_err);
+
+  // right ee to block
+  double right_block_err[3];
+  mju_sub3(right_block_err, ee_right, block_pos);
+  double right_block_dist = mju_norm3(right_block_err);
+
+  // left base to goal
+  double left_goal_err[3];
+  mju_sub3(left_goal_err, base_left, goal_pos);
+  double left_goal_dist = mju_norm3(left_goal_err);
+
+  // right base to goal
+  double right_goal_err[3];
+  mju_sub3(right_goal_err, base_right, goal_pos);
+  double right_goal_dist = mju_norm3(right_goal_err);
+  
+  double left_reach_scale = 1.0;
+  double right_reach_scale = 1.0;
+
+  if (left_goal_dist < right_goal_dist && left_block_dist < right_block_dist) {
+    right_reach_scale *= 0.1;
+  } else if (left_goal_dist < right_goal_dist &&
+             left_block_dist >= right_block_dist) {
+    left_reach_scale *= 0.1;
+  } else if (left_goal_dist >= right_goal_dist &&
+             left_block_dist >= right_block_dist) {
+    left_reach_scale *= 0.1;
+  } else if (left_goal_dist >= right_goal_dist &&
+             left_block_dist < right_block_dist) {
+    right_reach_scale *= 0.1;
+  }
+
+  // ----- Effort ----- //
+  mju_sub(residual + counter, data->ctrl, model->key_qpos, 6);
+  residual[counter + 6] = 0.1 * (data->ctrl[6] - 0.057);
+  counter += 7;
+
+  mju_sub(residual + counter, data->ctrl + 7, model->key_qpos + 8, 6);
+  residual[counter + 6] = 0.1 * (data->ctrl[13] - 0.057);
+  counter += 7;
 
   // ----- Left reach ----- //
-  double* ee_left = SensorByName(model, data, "ee_left");
   mju_sub3(residual + counter, block_pos, ee_left);
+  mju_scl3(residual + counter, residual + counter, left_reach_scale);
   counter += 3;
 
   // ----- Right reach ----- //
-  double* ee_right = SensorByName(model, data, "ee_right");
   mju_sub3(residual + counter, block_pos, ee_right);
+  mju_scl3(residual + counter, residual + counter, right_reach_scale);
   counter += 3;
+
+  // ----- Block position ----- //
+  mju_sub3(residual + counter, block_pos, goal_pos);
+  counter += 3;
+
+  
 
   // ----- Joint velocity ----- //
   // mju_copy(residual + counter, data->qvel, 16);
@@ -102,8 +159,8 @@ void Bimanual::ResidualFn::Residual(const mjModel* model, const mjData* data,
   counter += 3;
 
   // ----- Nominal pose ----- //
-  mju_sub(residual + counter, data->qpos, model->key_qpos, 16);
-  counter += 16;
+  // mju_sub(residual + counter, data->qpos, model->key_qpos, 8);
+  // counter += 8;
 
   // sensor dim sanity check
   CheckSensorDim(model, counter);

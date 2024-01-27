ed056d006741b98fa77a2ee3574d3e491bd84be4
taylor howell <taylor.athaniel.howell@gmail.com>
2024-01-27 10:02:15 -0700

    approximate remaining unscramble cost

--- mjpc/tasks/cube/solve.h
+++ mjpc/tasks/cube/solve.h
@@ -32,14 +32,18 @@ class CubeSolve : public Task {
 
   class ResidualFn : public BaseResidualFn {
    public:
-    explicit ResidualFn(const CubeSolve* task, int current_mode = 0)
-        : BaseResidualFn(task), current_mode_(current_mode) {}
+    explicit ResidualFn(const CubeSolve* task, int current_mode = 0,
+                        int goal_index = 0)
+        : BaseResidualFn(task),
+          current_mode_(current_mode),
+          goal_index_(goal_index) {}
     void Residual(const mjModel* model, const mjData* data,
                   double* residual) const override;
 
    private:
     friend class CubeSolve;
     int current_mode_ = 0;
+    int goal_index_ = 0;
   };
 
   CubeSolve();
@@ -57,7 +61,8 @@ class CubeSolve : public Task {
 
  protected:
   std::unique_ptr<mjpc::ResidualFn> ResidualLocked() const override {
-    return std::make_unique<ResidualFn>(this, residual_.current_mode_);
+    return std::make_unique<ResidualFn>(this, residual_.current_mode_,
+                                        residual_.goal_index_);
   }
   ResidualFn* InternalResidual() override { return &residual_; }
 

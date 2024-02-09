49388aaa7ff50ad920d4e3621aa603acac3c8c21
taylor howell <taylor.athaniel.howell@gmail.com>
2024-02-09 09:56:16 -0700

    assert

--- python/setup.py
+++ python/setup.py
@@ -150,6 +150,7 @@ class CopyTaskAssetsCommand(setuptools.Command):
 
   def run(self):
     mjpc_tasks_path = Path(__file__).parent.parent / "build" / "mjpc" / "tasks"
+    assert mjpc_tasks_path.exists(), "Build MJPC before installing Python API"
     source_paths = (
       tuple(mjpc_tasks_path.rglob("*.xml"))
       + tuple(mjpc_tasks_path.rglob("*.png"))

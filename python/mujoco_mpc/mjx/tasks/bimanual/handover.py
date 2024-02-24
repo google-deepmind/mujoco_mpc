8dbee8d566722a4acc809ddc648d7553e9cbc8b8
taylor howell <taylor.athaniel.howell@gmail.com>
2024-02-24 13:24:59 -0500

    minor changes

--- python/mujoco_mpc/mjx/tasks/bimanual/handover.py
+++ python/mujoco_mpc/mjx/tasks/bimanual/handover.py
@@ -18,8 +18,7 @@ import jax
 from jax import numpy as jp
 import mujoco
 from mujoco import mjx
-from mujoco_mpc.mjx import predictive_sampling
-
+from typing import Callable
 
 def bring_to_target(m: mjx.Model, d: mjx.Data) -> jax.Array:
   """Returns cost for bimanual bring to target task."""
@@ -48,7 +47,7 @@ def bring_to_target(m: mjx.Model, d: mjx.Data) -> jax.Array:
 
 
 def get_models_and_cost_fn() -> (
-    tuple[mujoco.MjModel, mujoco.MjModel, predictive_sampling.CostFn]
+    tuple[mujoco.MjModel, mujoco.MjModel, Callable[[mjx.Model, mjx.Data], jax.Array]]
 ):
   """Returns a tuple of the model and the cost function."""
   path = epath.Path(
--- python/mujoco_mpc/mjx/visualize.py
+++ python/mujoco_mpc/mjx/visualize.py
@@ -12,12 +12,12 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 # ==============================================================================
-
+# %%
 import matplotlib.pyplot as plt
 import mediapy
 import mujoco
-from mujoco_mpc.mjx import predictive_sampling
-from mujoco_mpc.mjx.tasks.bimanual import handover
+import predictive_sampling
+from tasks.bimanual import handover
 # %%
 nsteps = 500
 steps_per_plan = 4

d6a6a2952a45d4d4ec17b3878d7960320f82078d
Tom Erez <etom@google.com>
2024-03-19 10:32:41 -0700

    Refactor MJXPC for a single jit.
    PiperOrigin-RevId: 617218876
    Change-Id: I190916825c2ff3a566a6b42ede3d6de9a755e700

--- python/mujoco_mpc/mjx/predictive_sampling.py
+++ python/mujoco_mpc/mjx/predictive_sampling.py
@@ -17,9 +17,9 @@
 
 from typing import Callable, Tuple
 
+from brax.base import State
 import jax
 from jax import numpy as jnp
-import mujoco
 from mujoco import mjx
 from mujoco.mjx._src import dataclasses
 
@@ -30,7 +30,7 @@ class Planner(dataclasses.PyTreeNode):
   """Predictive sampling planner.
 
   Attributes:
-    model: MuJoCo model
+    model: MJX model
     cost: function returning per-timestep cost
     noise_scale: standard deviation of zero-mean Gaussian
     horizon: planning duration (steps)
@@ -40,14 +40,13 @@ class Planner(dataclasses.PyTreeNode):
   """
   model: mjx.Model
   cost: CostFn
-  noise_scale: jax.Array
+  noise_scale: float
   horizon: int
   nspline: int
   nsample: int
   interp: str
 
 
-@jax.jit
 def _rollout(p: Planner, d: mjx.Data, policy: jax.Array) -> jax.Array:
   """Expand the policy into actions and roll out dynamics and cost."""
   actions = get_actions(p, policy)
@@ -76,7 +75,6 @@ def get_actions(p: Planner, policy: jax.Array) -> jax.Array:
     raise ValueError(f'unimplemented interpolation method: {p.interp}')
 
   return actions
-v_get_actions = jax.vmap(get_actions, in_axes=[None, 0])
 
 
 def improve_policy(
@@ -105,13 +103,12 @@ def resample(p: Planner, policy: jax.Array, steps_per_plan: int) -> jax.Array:
   if p.interp == 'zero':
     return policy  # assuming steps_per_plan < splinesteps
   elif p.interp == 'linear':
-    actions = v_get_actions(p, policy)
+    actions = get_actions(p, policy)
     roll = steps_per_plan
-    actions = jnp.roll(actions, -roll, axis=1)
-    actions = actions.at[:, -roll:, :].set(actions[:, [-1], :])
+    actions = jnp.roll(actions, -roll, axis=-2)
+    actions = actions.at[..., -roll:, :].set(actions[..., [-1], :])
     idx = jnp.floor(jnp.linspace(0, p.horizon, p.nspline)).astype(int)
-    return actions[:, idx, :]
-
+    return actions[..., idx, :]
   return policy
 
 
@@ -119,80 +116,47 @@ def set_state(d, state):
   return d.replace(
       time=state.time, qpos=state.qpos, qvel=state.qvel, act=state.act,
       ctrl=state.ctrl)
-set_states = jax.vmap(set_state, in_axes=[0, 0])
 
-
-def receding_horizon_control(
+def mpc_rollout(
+    nsteps,
+    steps_per_plan,
     p: Planner,
     init_policy,
     rng,
-    plan_model_cpu,
-    sim_model_cpu,
-    nsteps,
-    nplans,
-    steps_per_plan,
+    sim_model,
+    sim_data,
 ):
-  """Receding horizon optimization, all nplans start from same keyframe."""
-  plan_data = mujoco.MjData(plan_model_cpu)
-  plan_data = mjx.put_data(plan_model_cpu, plan_data)
-  m = mjx.put_model(plan_model_cpu)
-  p = p.replace(model=m)
-
-  sim_data = mujoco.MjData(sim_model_cpu)
-  mujoco.mj_resetDataKeyframe(sim_model_cpu, sim_data, 0)
-  # without kinematics, the first cost is off:
-  mujoco.mj_forward(sim_model_cpu, sim_data)
-  sim_data = mjx.put_data(sim_model_cpu, sim_data)
-  sim_model = mjx.put_model(sim_model_cpu)
-
-  multi_actions = v_get_actions(p, init_policy)
-  first_actions = multi_actions[:, 0, :]  # just the first actions
-  # duplicate data for each plan
-  def set_action(data, action):
-    return data.replace(ctrl=action)
-
-  duplicate_data = jax.vmap(set_action, in_axes=[None, 0], out_axes=0)
-  sim_datas = duplicate_data(sim_data, first_actions)
-  plan_datas = duplicate_data(plan_data, first_actions)
-
-  def step_and_cost(data, action):
-    data = data.replace(ctrl=action)
-    cost = p.cost(sim_model, data)
-    data = mjx.step(sim_model, data)
-    return data, cost
-
-  multi_step = jax.vmap(step_and_cost, in_axes=[0, 0])
-
-  def step_cost_traj(datas, actions):
-    data, cost = multi_step(datas, actions)
-    return data, (cost, data.qpos)
-
-  improve_fn = jax.vmap(improve_policy, in_axes=(None, 0, 0, 0))
+  """Receding horizon optimization starting from sim_data's state."""
+  plan_data = mjx.make_data(p.model)
 
   def plan_and_step(carry, rng):
-    sim_datas, policy = carry
+    sim_data, policy = carry
     policy = resample(p, policy, steps_per_plan)
-    policy, _ = improve_fn(
+    policy, _ = improve_policy(
         p,
-        set_states(plan_datas, sim_datas),
+        set_state(plan_data, sim_data),
         policy,
-        jax.random.split(rng, nplans),
+        rng,
     )
-    multi_actions = v_get_actions(p, policy)
-    sim_datas, (cost, traj) = jax.lax.scan(
-        step_cost_traj,
-        sim_datas,
-        jnp.transpose(multi_actions[:, :steps_per_plan, :], (1, 0, 2)),
+    def step(d, action):
+      d = d.replace(ctrl=action)
+      cost = p.cost(sim_model, d)
+      d = mjx.step(sim_model, d)
+      return d, (
+          cost,
+          State(q=d.qpos, qd=d.qvel, x=None, xd=None, contact=None),
+      )
+    actions = get_actions(p, policy)
+    sim_data, (cost, traj) = jax.lax.scan(
+        step,
+        sim_data,
+        actions[:steps_per_plan, :],
     )
-    return (sim_datas, policy), (cost, traj)
+    return (sim_data, policy), (cost, traj)
 
-  (_, final_policy), (costs, trajs) = jax.lax.scan(
+  (sim_data, final_policy), (costs, trajs) = jax.lax.scan(
       plan_and_step,
-      (sim_datas, init_policy),
+      (sim_data, init_policy),
       jax.random.split(rng, nsteps // steps_per_plan),
   )
-  return (
-      trajs.reshape(nsteps, nplans, sim_model.nq).transpose(1, 0, 2),
-      costs.reshape(nsteps, nplans).transpose(),
-      final_policy,
-  )
+  return sim_data, final_policy, costs.flatten(), trajs

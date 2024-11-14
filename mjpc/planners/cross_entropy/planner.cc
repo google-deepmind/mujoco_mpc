google-deepmind/mujoco_mpc

commit 734bda29a09b238fc298e9a0ee0755ed33c054d7

Use the new TimeSpline to implement existing iLQS and Sampling planners.
They still perform spline remapping and don't make use of the DropBefore feature of TimeSpline.

PiperOrigin-RevId: 622801850
Change-Id: I88601264a2647c428ff2b054e7752f423839fae0

mjpc/planners/cross_entropy/planner.cc
@@ -20,10 +20,12 @@
 #include <shared_mutex>
 
 #include <absl/random/random.h>
+#include <absl/types/span.h>
 #include <mujoco/mujoco.h>
 #include "mjpc/array_safety.h"
 #include "mjpc/planners/planner.h"
 #include "mjpc/planners/sampling/planner.h"
+#include "mjpc/spline/spline.h"
 #include "mjpc/states/state.h"
 #include "mjpc/task.h"
 #include "mjpc/threadpool.h"
@@ -33,6 +35,7 @@
 namespace mjpc {
 
 namespace mju = ::mujoco::util_mjpc;
+using mjpc::spline::TimeSpline;
 
 // initialize data and settings
 void CrossEntropyPlanner::Initialize(mjModel* model, const Task& task) {
@@ -159,6 +162,8 @@ void CrossEntropyPlanner::SetState(const State& state) {
 
 // optimize nominal policy using random sampling
 void CrossEntropyPlanner::OptimizePolicy(int horizon, ThreadPool& pool) {
+  resampled_policy.plan.SetInterpolation(interpolation_);
+
   // if num_trajectory_ has changed, use it in this new iteration.
   // num_trajectory_ might change while this function runs. Keep it constant
   // for the duration of this function.
@@ -172,7 +177,6 @@ void CrossEntropyPlanner::OptimizePolicy(int horizon, ThreadPool& pool) {
   ResizeMjData(model, pool.NumThreads());
 
   // copy nominal policy
-  policy.num_parameters = model->nu * policy.num_spline_points;
   {
     const std::shared_lock<std::shared_mutex> lock(mtx_);
     resampled_policy.CopyFrom(policy, policy.num_spline_points);
@@ -211,7 +215,7 @@ void CrossEntropyPlanner::OptimizePolicy(int horizon, ThreadPool& pool) {
 
   // dimensions
   int num_spline_points = resampled_policy.num_spline_points;
-  int num_parameters = resampled_policy.num_parameters;
+  int num_parameters = num_spline_points * model->nu;
 
   // averaged return over elites
   double avg_return = 0.0;
@@ -225,8 +229,12 @@ void CrossEntropyPlanner::OptimizePolicy(int horizon, ThreadPool& pool) {
     int idx = trajectory_order[i];
 
     // add parameters
-    mju_addTo(parameters_scratch.data(),
-              candidate_policy[idx].parameters.data(), num_parameters);
+    for (int i = 0; i < num_spline_points; i++) {
+      TimeSpline::Node n = candidate_policy[idx].plan.NodeAt(i);
+      for (int j = 0; j < model->nu; j++) {
+        parameters_scratch[i * model->nu + j] += n.values()[j];
+      }
+    }
 
     // add total return
     avg_return += trajectory[idx].total_return;
@@ -240,13 +248,13 @@ void CrossEntropyPlanner::OptimizePolicy(int horizon, ThreadPool& pool) {
   // loop over elites to compute variance
   std::fill(variance.begin(), variance.end(), 0.0);  // reset variance to zero
   for (int t = 0; t < num_spline_points; t++) {
+    TimeSpline::Node n = candidate_policy[trajectory_order[0]].plan.NodeAt(t);
     for (int j = 0; j < model->nu; j++) {
       // average
       double p_avg = parameters_scratch[t * model->nu + j];
       for (int i = 0; i < n_elite; i++) {
         // candidate parameter
-        double pi =
-            candidate_policy[trajectory_order[i]].parameters[t * model->nu + j];
+        double pi = n.values()[j];
         double diff = pi - p_avg;
         variance[t * model->nu + j] += diff * diff / (n_elite - 1);
       }
@@ -256,7 +264,14 @@ void CrossEntropyPlanner::OptimizePolicy(int horizon, ThreadPool& pool) {
   // update
   {
     const std::shared_lock<std::shared_mutex> lock(mtx_);
-    policy.CopyParametersFrom(parameters_scratch, times_scratch);
+    policy.plan.Clear();
+    policy.plan.SetInterpolation(interpolation_);
+    for (int t = 0; t < num_spline_points; t++) {
+      absl::Span<const double> values =
+          absl::MakeConstSpan(parameters_scratch.data() + t * model->nu,
+                              parameters_scratch.data() + (t + 1) * model->nu);
+      policy.plan.AddNode(times_scratch[t], values);
+    }
   }
 
   // improvement: compare nominal to elite average
@@ -298,7 +313,6 @@ void CrossEntropyPlanner::ActionFromPolicy(double* action, const double* state,
 // update policy via resampling
 void CrossEntropyPlanner::ResamplePolicy(int horizon) {
   // dimensions
-  int num_parameters = resampled_policy.num_parameters;
   int num_spline_points = resampled_policy.num_spline_points;
 
   // time
@@ -315,15 +329,14 @@ void CrossEntropyPlanner::ResamplePolicy(int horizon) {
   }
 
   // copy resampled policy parameters
-  mju_copy(resampled_policy.parameters.data(), parameters_scratch.data(),
-           num_parameters);
-  mju_copy(resampled_policy.times.data(), times_scratch.data(),
-           num_spline_points);
-
-  LinearRange(resampled_policy.times.data(), time_shift,
-              resampled_policy.times[0], num_spline_points);
-
-  resampled_policy.representation = policy.representation;
+  resampled_policy.plan.Clear();
+  for (int t = 0; t < num_spline_points; t++) {
+    absl::Span<const double> values =
+        absl::MakeConstSpan(parameters_scratch.data() + t * model->nu,
+                            parameters_scratch.data() + (t + 1) * model->nu);
+    resampled_policy.plan.AddNode(times_scratch[t], values);
+  }
+  resampled_policy.plan.SetInterpolation(policy.plan.Interpolation());
 }
 
 // add random noise to nominal policy
@@ -333,7 +346,7 @@ void CrossEntropyPlanner::AddNoiseToPolicy(int i, double std_min) {
 
   // dimensions
   int num_spline_points = candidate_policy[i].num_spline_points;
-  int num_parameters = candidate_policy[i].num_parameters;
+  int num_parameters = num_spline_points * model->nu;
 
   // sampling token
   absl::BitGen gen_;
@@ -350,14 +363,13 @@ void CrossEntropyPlanner::AddNoiseToPolicy(int i, double std_min) {
         gen_, 0.0, std::max(std::sqrt(variance[k]), std_min));
   }
 
-  // add noise
-  mju_addTo(candidate_policy[i].parameters.data(), DataAt(noise, shift),
-            num_parameters);
-
-  // clamp parameters
-  for (int t = 0; t < num_spline_points; t++) {
-    Clamp(DataAt(candidate_policy[i].parameters, t * model->nu),
-          model->actuator_ctrlrange, model->nu);
+  for (int k = 0; k < candidate_policy[i].plan.Size(); k++) {
+    TimeSpline::Node n = candidate_policy[i].plan.NodeAt(k);
+    // add noise
+    mju_addTo(n.values().data(), DataAt(noise, shift + k * model->nu),
+              model->nu);
+    // clamp parameters
+    Clamp(n.values().data(), model->actuator_ctrlrange, model->nu);
   }
 
   // end timer
@@ -385,8 +397,8 @@ void CrossEntropyPlanner::Rollouts(int num_trajectory, int horizon,
         const std::shared_lock<std::shared_mutex> lock(s.mtx_);
         s.candidate_policy[i].CopyFrom(s.resampled_policy,
                                        s.resampled_policy.num_spline_points);
-        s.candidate_policy[i].representation =
-            s.resampled_policy.representation;
+        s.candidate_policy[i].plan.SetInterpolation(
+            s.resampled_policy.plan.Interpolation());
 
         // sample noise
         s.AddNoiseToPolicy(i, std_min);
@@ -473,7 +485,7 @@ void CrossEntropyPlanner::Traces(mjvScene* scn) {
 void CrossEntropyPlanner::GUI(mjUI& ui) {
   mjuiDef defCrossEntropy[] = {
       {mjITEM_SLIDERINT, "Rollouts", 2, &num_trajectory_, "0 1"},
-      {mjITEM_SELECT, "Spline", 2, &policy.representation,
+      {mjITEM_SELECT, "Spline", 2, &interpolation_,
        "Zero\nLinear\nCubic"},
       {mjITEM_SLIDERINT, "Spline Pts", 2, &policy.num_spline_points, "0 1"},
       {mjITEM_SLIDERNUM, "Init. Std", 2, &std_initial_, "0 1"},

mjpc/planners/ilqs/planner.cc
@@ -15,7 +15,9 @@
 #include "mjpc/planners/ilqs/planner.h"
 
 #include <chrono>
+#include <vector>
 
+#include <absl/types/span.h>
 #include <mujoco/mujoco.h>
 #include "mjpc/array_safety.h"
 #include "mjpc/planners/ilqg/planner.h"
@@ -102,13 +104,11 @@ void iLQSPlanner::OptimizePolicy(int horizon, ThreadPool& pool) {
         1.0e-5);
 
     // get spline points
+    spline_times_cache.clear();
     for (int t = 0; t < num_spline_points; t++) {
-      sampling.policy.times[t] = nominal_time;
+      spline_times_cache.push_back(nominal_time);
       nominal_time += time_shift;
     }
-
-    LinearRange(sampling.policy.times.data(), time_shift,
-                sampling.policy.times[0], num_spline_points);
 
     // linear system solve
     if (dim_actions != sampling.model->nu * (horizon - 1) ||
@@ -118,8 +118,8 @@ void iLQSPlanner::OptimizePolicy(int horizon, ThreadPool& pool) {
       dim_actions = sampling.model->nu * (horizon - 1);
 
       // compute parameter to action mapping
-      mappings[sampling.policy.representation]->Compute(
-          sampling.policy.times, num_spline_points,
+      mappings[sampling.policy.plan.Interpolation()]->Compute(
+          spline_times_cache, num_spline_points,
           ilqg.candidate_policy[0].trajectory.times.data(), horizon - 1);
 
       // ----- compute inverse mapping ----- //
@@ -129,7 +129,7 @@ void iLQSPlanner::OptimizePolicy(int horizon, ThreadPool& pool) {
       inversemappingT.resize(dim_actions * dim_parameters);
 
       // M = A' A
-      double* mapping = mappings[sampling.policy.representation]->Get();
+      double* mapping = mappings[sampling.policy.plan.Interpolation()]->Get();
       mju_mulMatTMat(inversemapping_cache.data(), mapping, mapping, dim_actions,
                      dim_parameters, dim_parameters);
 
@@ -149,14 +149,23 @@ void iLQSPlanner::OptimizePolicy(int horizon, ThreadPool& pool) {
     }
 
     // compute parameters from actions via inverse mapping
-    mju_mulMatVec(sampling.policy.parameters.data(), inversemapping.data(),
+    spline_parameters_cache.resize(dim_parameters);
+    mju_mulMatVec(spline_parameters_cache.data(), inversemapping.data(),
                   ilqg.candidate_policy[0].trajectory.actions.data(),
                   dim_parameters, dim_actions);
 
     // clamp parameters
     for (int t = 0; t < num_spline_points; t++) {
-      Clamp(DataAt(sampling.policy.parameters, t * sampling.model->nu),
+      Clamp(DataAt(spline_parameters_cache, t * sampling.model->nu),
             sampling.model->actuator_ctrlrange, sampling.model->nu);
+    }
+    sampling.policy.plan.Clear();
+    for (int t = 0; t < num_spline_points; t++) {
+      sampling.policy.plan.AddNode(
+          spline_times_cache[t],
+          absl::MakeConstSpan(
+              spline_parameters_cache.data() + t * sampling.model->nu,
+              sampling.model->nu));
     }
   }
 

mjpc/planners/sample_gradient/planner.cc
@@ -25,6 +25,8 @@
 #include "mjpc/planners/planner.h"
 #include "mjpc/planners/policy.h"
 #include "mjpc/planners/sampling/planner.h"
+#include "mjpc/planners/sampling/policy.h"
+#include "mjpc/spline/spline.h"
 #include "mjpc/states/state.h"
 #include "mjpc/task.h"
 #include "mjpc/threadpool.h"
@@ -34,6 +36,8 @@
 namespace mjpc {
 
 namespace mju = ::mujoco::util_mjpc;
+using mjpc::spline::SplineInterpolation;
+using mjpc::spline::TimeSpline;
 
 // initialize data and settings
 void SampleGradientPlanner::Initialize(mjModel* model, const Task& task) {
@@ -54,6 +58,9 @@ void SampleGradientPlanner::Initialize(mjModel* model, const Task& task) {
 
   // set number of trajectories to rollout
   num_trajectory_ = GetNumberOrDefault(10, model, "sampling_trajectories");
+
+  interpolation_ = GetNumberOrDefault(SplineInterpolation::kCubicSpline, model,
+                                      "sampling_representation");
 
   // set number of gradient trajectories to rollout
   num_gradient_ = GetNumberOrDefault(0, model, "sample_gradient_trajectories");
@@ -83,12 +90,10 @@ void SampleGradientPlanner::Allocate() {
   resampled_policy.Allocate(model, *task, kMaxTrajectoryHorizon);
   previous_policy.Allocate(model, *task, kMaxTrajectoryHorizon);
 
-  // scratch
-  parameters_scratch.resize(num_max_parameter);
-  times_scratch.resize(kMaxTrajectoryHorizon);
-
   // noise
   noise.resize(kMaxTrajectory * (model->nu * kMaxTrajectoryHorizon));
+
+  plan_scratch = TimeSpline(/*dim=*/model->nu);
 
   // need to initialize an arbitrary order of the trajectories
   trajectory_order.resize(kMaxTrajectory);
@@ -127,8 +132,7 @@ void SampleGradientPlanner::Reset(int horizon,
   previous_policy.Reset(horizon, initial_repeated_action);
 
   // scratch
-  std::fill(parameters_scratch.begin(), parameters_scratch.end(), 0.0);
-  std::fill(times_scratch.begin(), times_scratch.end(), 0.0);
+  plan_scratch.Clear();
 
   // noise
   std::fill(noise.begin(), noise.end(), 0.0);
@@ -180,23 +184,21 @@ void SampleGradientPlanner::OptimizePolicy(int horizon, ThreadPool& pool) {
 
   // copy nominal policy
   int num_spline_points = policy.num_spline_points;
-  PolicyRepresentation representation = policy.representation;
-  policy.num_parameters = model->nu * num_spline_points;
+  policy.plan.SetInterpolation(interpolation_);
   {
     const std::shared_lock<std::shared_mutex> lock(mtx_);
     resampled_policy.CopyFrom(policy, num_spline_points);
   }
 
   // resample nominal policy to current time
-  this->ResamplePolicy(resampled_policy, horizon, num_spline_points,
-                       representation);
+  this->ResamplePolicy(resampled_policy, horizon, num_spline_points);
 
   // resample gradient policies to current time
   // TODO(taylor): a bit faster to do in Rollouts, but needs more scratch to be
   // memory safe
   for (int i = 0; i < num_gradient; i++) {
     this->ResamplePolicy(candidate_policy[num_noisy + i], horizon,
-                         num_spline_points, representation);
+                         num_spline_points);
   }
 
   // ----- roll out noisy policies ----- //
@@ -248,8 +250,7 @@ void SampleGradientPlanner::OptimizePolicy(int horizon, ThreadPool& pool) {
   // update
   {
     const std::shared_lock<std::shared_mutex> lock(mtx_);
-    policy.CopyParametersFrom(candidate_policy[winner].parameters,
-                              times_scratch);
+    policy.SetPlan(candidate_policy[winner].plan);
   }
 
   // improvement: compare nominal to winner
@@ -299,10 +300,8 @@ void SampleGradientPlanner::ActionFromPolicy(double* action,
 
 // update policy via resampling
 void SampleGradientPlanner::ResamplePolicy(
-    SamplingPolicy& policy, int horizon, int num_spline_points,
-    PolicyRepresentation representation) {
+    SamplingPolicy& policy, int horizon, int num_spline_points) {
   // dimension
-  int num_parameters = model->nu * num_spline_points;
 
   // time
   double nominal_time = time;
@@ -310,29 +309,20 @@ void SampleGradientPlanner::ResamplePolicy(
       (horizon - 1) * model->opt.timestep / (num_spline_points - 1), 1.0e-5);
 
   // get spline points
+  plan_scratch.Clear();
+  plan_scratch.Reserve(num_spline_points);
+  plan_scratch.SetInterpolation(policy.plan.Interpolation());
   for (int t = 0; t < num_spline_points; t++) {
-    times_scratch[t] = nominal_time;
-    policy.Action(DataAt(parameters_scratch, t * model->nu), nullptr,
-                  nominal_time);
+    TimeSpline::Node node = plan_scratch.AddNode(nominal_time);
+    policy.Action(node.values().data(), /*state=*/nullptr, nominal_time);
     nominal_time += time_shift;
   }
 
   // copy resampled policy parameters
-  mju_copy(policy.parameters.data(), parameters_scratch.data(),
-           num_parameters);
-  mju_copy(policy.times.data(), times_scratch.data(),
-           num_spline_points);
-
-  // time step linear range
-  LinearRange(policy.times.data(), time_shift,
-              policy.times[0], num_spline_points);
+  policy.SetPlan(plan_scratch);
 
   // set dimensions
-  policy.num_parameters = num_parameters;
   policy.num_spline_points = num_spline_points;
-
-  // representation
-  policy.representation = representation;
 }
 
 // add random noise to nominal policy
@@ -342,8 +332,6 @@ void SampleGradientPlanner::AddNoiseToPolicy(int i) {
 
   // dimensions
   int num_spline_points = candidate_policy[i].num_spline_points;
-  int num_parameters = candidate_policy[i].num_parameters;
-
   // sampling token
   absl::BitGen gen_;
 
@@ -351,18 +339,15 @@ void SampleGradientPlanner::AddNoiseToPolicy(int i) {
   int shift = i * (model->nu * kMaxTrajectoryHorizon);
 
   // sample noise
-  for (int k = 0; k < num_parameters; k++) {
+  for (int k = 0; k < num_spline_points * model->nu; k++) {
     noise[k + shift] = absl::Gaussian<double>(gen_, 0.0, 1.0);
   }
 
-  // add noise
-  mju_addToScl(candidate_policy[i].parameters.data(), DataAt(noise, shift),
-               noise_exploration, num_parameters);
-
-  // clamp parameters
-  for (int t = 0; t < num_spline_points; t++) {
-    Clamp(DataAt(candidate_policy[i].parameters, t * model->nu),
-          model->actuator_ctrlrange, model->nu);
+  for (int j = 0; j < num_spline_points; j++) {
+    TimeSpline::Node node = candidate_policy[i].plan.NodeAt(j);
+    mju_addToScl(node.values().data(), DataAt(noise, j * model->nu + shift),
+                 noise_exploration, model->nu);
+    Clamp(node.values().data(), model->actuator_ctrlrange, model->nu);
   }
 
   // end timer
@@ -388,8 +373,6 @@ void SampleGradientPlanner::Rollouts(int num_trajectory, int num_gradient,
         // copy nominal policy
         s.candidate_policy[i].CopyFrom(s.resampled_policy,
                                        s.resampled_policy.num_spline_points);
-        s.candidate_policy[i].representation =
-            s.resampled_policy.representation;
 
         // noisy nominal policy
         if (i > idx_nominal) s.AddNoiseToPolicy(i);
@@ -421,8 +404,8 @@ void SampleGradientPlanner::GradientCandidates(int num_trajectory,
   if (num_gradient < 1) return;
 
   // number of parameters
-  int num_parameters = resampled_policy.num_parameters;
   int num_spline_points = resampled_policy.num_spline_points;
+  int num_parameters = num_spline_points * model->nu;
 
   // cache old gradient
   mju_copy(gradient_previous.data(), gradient.data(), num_parameters);
@@ -490,24 +473,21 @@ void SampleGradientPlanner::GradientCandidates(int num_trajectory,
   for (int i = num_noisy; i < num_trajectory; i++) {
     // copy nominal policy
     candidate_policy[i].CopyFrom(resampled_policy, num_spline_points);
-    candidate_policy[i].representation = resampled_policy.representation;
 
     // scaling
     double scaling = step_size_[i - num_noisy] / noise_exploration;
 
     // gradient step
-    mju_addToScl(candidate_policy[i].parameters.data(), gradient.data(),
-                 -scaling * gradient_filter, num_parameters);
+    for (int t = 0; t < candidate_policy[i].plan.Size(); t++) {
+      TimeSpline::Node n = candidate_policy[i].plan.NodeAt(t);
+      mju_addToScl(n.values().data(), gradient.data() + t * model->nu,
+                   -scaling * gradient_filter, model->nu);
 
-    // TODO(taylor): resample the gradient_previous?
-    mju_addToScl(candidate_policy[i].parameters.data(),
-                 gradient_previous.data(), -scaling * (1.0 - gradient_filter),
-                 num_parameters);
-
-    // clamp parameters
-    for (int t = 0; t < num_spline_points; t++) {
-      Clamp(DataAt(candidate_policy[i].parameters, t * model->nu),
-            model->actuator_ctrlrange, model->nu);
+      // TODO(taylor): resample the gradient_previous?
+      mju_addToScl(n.values().data(), gradient_previous.data() + t * model->nu,
+                   -scaling * (1.0 - gradient_filter), model->nu);
+      // clamp parameters
+      Clamp(n.values().data(), model->actuator_ctrlrange, model->nu);
     }
   }
 }
@@ -582,8 +562,7 @@ void SampleGradientPlanner::Traces(mjvScene* scn) {
 void SampleGradientPlanner::GUI(mjUI& ui) {
   mjuiDef defSampleGradient[] = {
       {mjITEM_SLIDERINT, "Rollouts", 2, &num_trajectory_, "0 1"},
-      {mjITEM_SELECT, "Spline", 2, &policy.representation,
-       "Zero\nLinear\nCubic"},
+      {mjITEM_SELECT, "Spline", 2, &interpolation_, "Zero\nLinear\nCubic"},
       {mjITEM_SLIDERINT, "Spline Pts", 2, &policy.num_spline_points, "0 1"},
       {mjITEM_SLIDERNUM, "Noise Std.", 2, &noise_exploration, "0 1"},
       {mjITEM_SLIDERINT, "Grad. Rollouts", 2, &num_gradient_, "0 1"},

mjpc/planners/sampling/planner.cc
@@ -23,6 +23,7 @@
 #include "mjpc/array_safety.h"
 #include "mjpc/planners/planner.h"
 #include "mjpc/planners/sampling/policy.h"
+#include "mjpc/spline/spline.h"
 #include "mjpc/states/state.h"
 #include "mjpc/task.h"
 #include "mjpc/threadpool.h"
@@ -32,6 +33,8 @@
 namespace mjpc {
 
 namespace mju = ::mujoco::util_mjpc;
+using mjpc::spline::SplineInterpolation;
+using mjpc::spline::TimeSpline;
 
 // initialize data and settings
 void SamplingPlanner::Initialize(mjModel* model, const Task& task) {
@@ -52,6 +55,9 @@ void SamplingPlanner::Initialize(mjModel* model, const Task& task) {
   // set number of trajectories to rollout
   num_trajectory_ = GetNumberOrDefault(10, model, "sampling_trajectories");
 
+  interpolation_ = GetNumberOrDefault(SplineInterpolation::kCubicSpline, model,
+                                      "sampling_representation");
+
   if (num_trajectory_ > kMaxTrajectory) {
     mju_error_i("Too many trajectories, %d is the maximum allowed.",
                 kMaxTrajectory);
@@ -71,13 +77,9 @@ void SamplingPlanner::Allocate() {
   userdata.resize(model->nuserdata);
 
   // policy
-  int num_max_parameter = model->nu * kMaxTrajectoryHorizon;
   policy.Allocate(model, *task, kMaxTrajectoryHorizon);
   previous_policy.Allocate(model, *task, kMaxTrajectoryHorizon);
-
-  // scratch
-  parameters_scratch.resize(num_max_parameter);
-  times_scratch.resize(kMaxTrajectoryHorizon);
+  plan_scratch = TimeSpline(/*dim=*/model->nu);
 
   // noise
   noise.resize(kMaxTrajectory * (model->nu * kMaxTrajectoryHorizon));
@@ -106,8 +108,7 @@ void SamplingPlanner::Reset(int horizon,
   previous_policy.Reset(horizon, initial_repeated_action);
 
   // scratch
-  std::fill(parameters_scratch.begin(), parameters_scratch.end(), 0.0);
-  std::fill(times_scratch.begin(), times_scratch.end(), 0.0);
+  plan_scratch.Clear();
 
   // noise
   std::fill(noise.begin(), noise.end(), 0.0);
@@ -153,6 +154,7 @@ int SamplingPlanner::OptimizePolicyCandidates(int ncandidates, int horizon,
   auto rollouts_start = std::chrono::steady_clock::now();
 
   // simulate noisy policies
+  policy.plan.SetInterpolation(interpolation_);
   this->Rollouts(num_trajectory, horizon, pool);
 
   // sort candidate policies and trajectories by score
@@ -229,25 +231,31 @@ void SamplingPlanner::UpdateNominalPolicy(int horizon) {
 
   // set time
   double nominal_time = time;
-  double time_shift = mju_max(
-      (horizon - 1) * model->opt.timestep / (num_spline_points - 1), 1.0e-5);
+  double time_shift;
+  if (interpolation_ == spline::SplineInterpolation::kZeroSpline) {
+    time_shift = mju_max(
+        (horizon - 1) * model->opt.timestep / num_spline_points, 1.0e-5);
+  } else {
+    time_shift = mju_max(
+        (horizon - 1) * model->opt.timestep / (num_spline_points - 1), 1.0e-5);
+  }
+
+  plan_scratch.Clear();
+  plan_scratch.Reserve(num_spline_points);
+  plan_scratch.SetInterpolation(interpolation_);
 
   // get spline points
   for (int t = 0; t < num_spline_points; t++) {
-    times_scratch[t] = nominal_time;
-    candidate_policy[winner].Action(DataAt(parameters_scratch, t * model->nu),
-                               nullptr, nominal_time);
+    TimeSpline::Node node = plan_scratch.AddNode(nominal_time);
+    candidate_policy[winner].Action(node.values().data(), /*state=*/nullptr,
+                                    nominal_time);
     nominal_time += time_shift;
   }
 
   // update
   {
     const std::shared_lock<std::shared_mutex> lock(mtx_);
-    // parameters
-    policy.CopyParametersFrom(parameters_scratch, times_scratch);
-
-    LinearRange(policy.times.data(), time_shift, policy.times[0],
-                num_spline_points);
+    policy.SetPlan(plan_scratch);
   }
 }
 
@@ -256,34 +264,18 @@ void SamplingPlanner::AddNoiseToPolicy(int i) {
   // start timer
   auto noise_start = std::chrono::steady_clock::now();
 
-  // dimensions
-  int num_spline_points = candidate_policy[i].num_spline_points;
-  int num_parameters = candidate_policy[i].num_parameters;
-
   // sampling token
   absl::BitGen gen_;
 
-  // shift index
-  int shift = i * (model->nu * kMaxTrajectoryHorizon);
-
-  // sample noise
-  for (int t = 0; t < num_spline_points; t++) {
+  for (const TimeSpline::Node& node : candidate_policy[i].plan) {
     for (int k = 0; k < model->nu; k++) {
       double scale = 0.5 * (model->actuator_ctrlrange[2 * k + 1] -
                             model->actuator_ctrlrange[2 * k]);
-      noise[shift + t * model->nu + k] =
+      double noise =
           absl::Gaussian<double>(gen_, 0.0, scale * noise_exploration);
+      node.values()[k] += noise;
     }
-  }
-
-  // add noise
-  mju_addTo(candidate_policy[i].parameters.data(), DataAt(noise, shift),
-            num_parameters);
-
-  // clamp parameters
-  for (int t = 0; t < num_spline_points; t++) {
-    Clamp(DataAt(candidate_policy[i].parameters, t * model->nu),
-          model->actuator_ctrlrange, model->nu);
+    Clamp(node.values().data(), model->actuator_ctrlrange, model->nu);
   }
 
   // end timer
@@ -296,8 +288,6 @@ void SamplingPlanner::Rollouts(int num_trajectory, int horizon,
   // reset noise compute time
   noise_compute_time = 0.0;
 
-  policy.num_parameters = model->nu * policy.num_spline_points;
-
   // random search
   int count_before = pool.GetCount();
   for (int i = 0; i < num_trajectory; i++) {
@@ -309,7 +299,6 @@ void SamplingPlanner::Rollouts(int num_trajectory, int horizon,
       {
         const std::shared_lock<std::shared_mutex> lock(s.mtx_);
         s.candidate_policy[i].CopyFrom(s.policy, s.policy.num_spline_points);
-        s.candidate_policy[i].representation = s.policy.representation;
       }
 
       // sample noise policy
@@ -392,7 +381,7 @@ void SamplingPlanner::Traces(mjvScene* scn) {
 void SamplingPlanner::GUI(mjUI& ui) {
   mjuiDef defSampling[] = {
       {mjITEM_SLIDERINT, "Rollouts", 2, &num_trajectory_, "0 1"},
-      {mjITEM_SELECT, "Spline", 2, &policy.representation,
+      {mjITEM_SELECT, "Spline", 2, &interpolation_,
        "Zero\nLinear\nCubic"},
       {mjITEM_SLIDERINT, "Spline Pts", 2, &policy.num_spline_points, "0 1"},
       {mjITEM_SLIDERNUM, "Noise Std", 2, &noise_exploration, "0 1"},

mjpc/agent.cc
@@ -70,7 +70,7 @@ Agent::Agent(const mjModel* model, std::shared_ptr<Task> task)
 // initialize data, settings, planners, state
 void Agent::Initialize(const mjModel* model) {
   // ----- model ----- //
-  if (model_) mj_deleteModel(model_);
+  mjModel* old_model = model_;
   model_ = mj_copyModel(nullptr, model);  // agent's copy of model
 
   // check for limits on all actuators
@@ -152,6 +152,12 @@ void Agent::Initialize(const mjModel* model) {
   // planner threads
   planner_threads_ =
       std::max(1, NumAvailableHardwareThreads() - 3 - 2 * estimator_threads_);
+
+  // delete the previous model after all the planners have been updated to use
+  // the new one.
+  if (old_model) {
+    mj_deleteModel(old_model);
+  }
 }
 
 // allocate memory

commit 868f357ca128543dcb43a84aa8bb9b4ee17aefe2

Correctly compute plan variance in cross-entropy planner.
Commit 734bda2 accidentally changed the variance computation from
```
sum((candidate_policy[idx] - avg)**2 for idx in elites) / (len(elites) - 1)
```

To:
```
sum((candidate_policy[0] - avg)**2 for idx in elites) / (len(elites) - 1)
```

I can't visually see a difference in performance on an in-hand reorientation task.
Fixes #350.

PiperOrigin-RevId: 696463663
Change-Id: I8f13e426d883cb01565d8f051ab29bf8a9a67471

mjpc/planners/cross_entropy/planner.cc
@@ -227,12 +227,13 @@ void CrossEntropyPlanner::OptimizePolicy(int horizon, ThreadPool& pool) {
   for (int i = 0; i < n_elite; i++) {
     // ordered trajectory index
     int idx = trajectory_order[i];
+    const TimeSpline& elite_plan = candidate_policy[idx].plan;
 
     // add parameters
-    for (int i = 0; i < num_spline_points; i++) {
-      TimeSpline::Node n = candidate_policy[idx].plan.NodeAt(i);
+    for (int t = 0; t < num_spline_points; t++) {
+      TimeSpline::ConstNode n = elite_plan.NodeAt(t);
       for (int j = 0; j < model->nu; j++) {
-        parameters_scratch[i * model->nu + j] += n.values()[j];
+        parameters_scratch[t * model->nu + j] += n.values()[j];
       }
     }
 
@@ -247,12 +248,15 @@ void CrossEntropyPlanner::OptimizePolicy(int horizon, ThreadPool& pool) {
 
   // loop over elites to compute variance
   std::fill(variance.begin(), variance.end(), 0.0);  // reset variance to zero
-  for (int t = 0; t < num_spline_points; t++) {
-    TimeSpline::Node n = candidate_policy[trajectory_order[0]].plan.NodeAt(t);
-    for (int j = 0; j < model->nu; j++) {
-      // average
-      double p_avg = parameters_scratch[t * model->nu + j];
-      for (int i = 0; i < n_elite; i++) {
+  for (int i = 0; i < n_elite; i++) {
+    int idx = trajectory_order[i];
+    const TimeSpline& elite_plan = candidate_policy[idx].plan;
+    for (int t = 0; t < num_spline_points; t++) {
+      TimeSpline::ConstNode n = elite_plan.NodeAt(t);
+      for (int j = 0; j < model->nu; j++) {
+        // average
+        double p_avg = parameters_scratch[t * model->nu + j];
+
         // candidate parameter
         double pi = n.values()[j];
         double diff = pi - p_avg;